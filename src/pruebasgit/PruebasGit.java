e424ff6575b712cfa5789f17b826ff10daa5e7b6
RaulGF <raulgf1982@gmail.com>
2021-03-17 21:31:40 +0100

    dia_1

---
+++ src/pruebasgit/PruebasGit.java
@@ -0,0 +1,20 @@
+package pruebasgit;
+
+/**
+ *
+ * @author Raúl
+ */
+public class PruebasGit {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo, by Raul");
+        
+        int a = 5;
+        
+        int b = 10;
+        
+        System.out.println(a * b);
+    }
+    
+}
